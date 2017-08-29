0ee40e759d0303b84f9a7c7f3a87a77eafbfe5f5
Maarten de Vries <maarten@de-vri.es>
2017-08-29 12:16:42 +0200

    Bump version to 0.1.3.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from glob import glob
 
 setup(
 	name='aprt',
-	version='0.1.2',
+	version='0.1.3',
 	description='Arch Linux Package Repository Tools',
 	url='http://github.com/delftrobotics/python-aprt',
 	author='Maarten de Vries',
